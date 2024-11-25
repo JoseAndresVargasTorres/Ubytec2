2b6871973aa7b07105fc65b6903188dd9f24f3e9
joseaam2020 <59858167+joseaam2020@users.noreply.github.com>
2024-11-25 05:35:37 -0600

    Ultimas compras

--- UbyTEC/src/app/admin/pages/gestionar-afiliaciones/gestionar-afiliaciones.component.ts
+++ UbyTEC/src/app/admin/pages/gestionar-afiliaciones/gestionar-afiliaciones.component.ts
@@ -5,12 +5,8 @@ import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { TableService } from '../../../Services/Table/table.service';
 import { ApiService } from '../../../Services/API/api.service';
-<<<<<<< HEAD
 import { forkJoin } from 'rxjs';
 import { ValidacionComercioControllerSQL } from '../../interfaces/comercioafiliado/Validacion_Comercio';
-=======
-import { concat, forkJoin } from 'rxjs';
->>>>>>> 3c6d26711e00a87a4444ac3a83b12d6c4370e1e2
 
 interface ValidacionComercio {
   id: string;
@@ -36,14 +32,6 @@ interface ComercioAfiliado {
   styleUrl: './gestionar-afiliaciones.component.css'
 })
 export class GestionarAfiliacionesComponent implements OnInit {
-<<<<<<< HEAD
-=======
-  solicitudes: ValidacionComercio[] = [];
-  comercios: ComercioAfiliado[] = [];
-  usarMock: boolean = false;
-  formRechazo: FormGroup;
-  solicitudSeleccionada: ValidacionComercio | null = null;
->>>>>>> 3c6d26711e00a87a4444ac3a83b12d6c4370e1e2
   displayedColumns: string[] = [];
   objects = [];
   columns: string[] = ['cedula_Juridica', 'nombre', 'correo'];
@@ -105,7 +93,6 @@ export class GestionarAfiliacionesComponent implements OnInit {
   async rechazarSolicitud(cedula: string) {
     const api1 = this.api_service.deleteData(`ValidacionComercioControllerSQL/${cedula}`);
     const api2 = this.api_service.deleteData(`ComercioAfiliado/${cedula}`);
-<<<<<<< HEAD
 
     forkJoin([api1, api2]).subscribe({
       next: res => {
@@ -113,15 +100,6 @@ export class GestionarAfiliacionesComponent implements OnInit {
         this.cargarDatos(); // Recargar datos después de rechazar
       },
       error: err => {console.error(err)}
-=======
-    const api3 = this.api_service.deleteData(`DireccionComercio/${cedula}`);
-
-    
-    concat([api1, api2, api3]).subscribe({
-      next: res => {
-        console.log(res);
-      }, error: err => {console.error(err)}
->>>>>>> 3c6d26711e00a87a4444ac3a83b12d6c4370e1e2
     })
   }
 }
